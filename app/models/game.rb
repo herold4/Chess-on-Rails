b677a0c55d91a38ebc266f154e878b44baefebc0
App Academy Student <herold4@gmail.com>
2014-09-27 17:48:52 -0700

    pawn promotion done, checkmate done, ui done

--- app/models/game.rb
+++ app/models/game.rb
@@ -16,9 +16,10 @@ class Game
   def process_move(s_pos, e_pos)
     c_color = (@whites_turn ? :white : :black)
     res = @board.move(s_pos, e_pos, c_color)
+    check_pawn_promotion
     @whites_turn = !@whites_turn if res == 'executed'
-    c_color = (@whites_turn ? :white : :black)
-    if res =='executed' && @board.in_check?(c_color)
+    next_color = (@whites_turn ? :white : :black)
+    if res =='executed' && @board.in_check?(next_color)
       res = "Player In Check!!" 
       res = "THAT LOOKS LIKE CHECKMATE!" if won?
     end
@@ -33,19 +34,12 @@ class Game
     end
   end
   
-  
-  def check_pawn_promotion(color)
-    class_hash = {"queen" => Queen, "rook" => Rook, "bishop" => Bishop, "knight" => Knight}
-    
+  def check_pawn_promotion
+    color = (@whites_turn ? :white : :black)
     pawns = @board.get_pieces(color).select { |piece| piece.is_a?(Pawn) }
     pawns.each do |pawn|
       if (color == :white && pawn.y == 0) || (color == :black && pawn.y == 7)
-
-        new_piece = gets.chomp.downcase
-        if !class_hash[new_piece]
-          raise 'Thats not a valid promotion'
-        end
-        @board[pawn.x, pawn.y] = class_hash[new_piece].new(pawn.color, @board, pawn.x, pawn.y)
+        @board[pawn.x, pawn.y] = Queen.new(pawn.color, @board, pawn.x, pawn.y)
       end
     end 
   end
