d4494e2e4062e7f1bc622e1ac4161617b4aa673d
App Academy Student <herold4@gmail.com>
2014-09-30 20:29:15 -0700

    drop table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930172829) do
+ActiveRecord::Schema.define(version: 20140930172859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,6 @@ ActiveRecord::Schema.define(version: 20140930172829) do
     t.string   "session_id"
     t.string   "white_moved"
     t.string   "black_moved"
-  end
-
-  create_table "players", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
