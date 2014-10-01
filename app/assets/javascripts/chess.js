54b0f7a4a78c6d0985070823f551cb0dd2b73de6
App Academy Student <herold4@gmail.com>
2014-09-30 22:17:52 -0700

    draggin piece off board

--- app/assets/javascripts/chess.js
+++ app/assets/javascripts/chess.js
@@ -1,15 +1,7 @@
-
-
-
-
-
 $(document).ready(function() {
   window.hitspace = false;
   function post(params) {
-      method = "post"; // Set method to post by default if not specified.
-      
-      // The rest of this code assumes you are not using a library.
-      // It can be made less wordy if you use one.
+      method = "post"; 
       var form = document.createElement("form");
       form.setAttribute("method", method);
       form.setAttribute("action", '/move');
@@ -20,7 +12,6 @@ $(document).ready(function() {
               hiddenField.setAttribute("type", "hidden");
               hiddenField.setAttribute("name", key);
               hiddenField.setAttribute("value", params[key]);
-
               form.appendChild(hiddenField);
            }
       }
@@ -29,10 +20,7 @@ $(document).ready(function() {
   }
   
   $('span').on('mousedown', function () {
-    
     window.pickedUp = event.target.id;
-    console.log('Picked Up:')
-    console.log(window.pickedUp)  
   })
   
   $('span').draggable({
@@ -43,21 +31,16 @@ $(document).ready(function() {
           start: window.pickedUp,
           landing: window.hitspace
         }
-        console.log('move data:')
-        console.log(data)
         window.hitspace = false;
         post(data);
       } else {
-        location.reload()
+        window.location.href = window.location.href
       }         
     }
   })
   $('.space').droppable({
     drop: function () {
       window.hitspace = this.id;
-      console.log('dropped to')
-      console.log(this.id)
-      
     }
   })
 });
